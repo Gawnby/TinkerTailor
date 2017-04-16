da8df95df86126f859c12417872d4671c098475f
Gawnby <gombaspeteer@gmail.com>
2017-04-16 09:17:15 +0200

    tinkertailor ready

--- tinkertailor.py
+++ tinkertailor.py
@@ -1,2 +1,19 @@
 class TinkerTailor:
-    pass
+
+    def __init__(self, steps, players):
+        self.steps = steps
+        self.players = players
+        self.final_result = []
+
+    def tinker_game(self):
+        index = 0
+        start_list = list(range(1, self.players + 1))
+        while start_list:
+            index = (index + (self.steps - 1)) % len(start_list)
+            self.final_result.append(start_list.pop(index))
+        print(self.final_result)
+
+
+
+play = TinkerTailor(3, 5)
+play.tinker_game()
